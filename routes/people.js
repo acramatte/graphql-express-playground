c92da4647873ce44d43102d92cde516c67bbcace
Alexis Cramatte <alexis.cramatte@comerge.net>
2016-10-23 23:49:01 +0200

    replace functions by arrow functions for router

--- routes/people.js
+++ routes/people.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const people = require('../people.json');
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.send(people);
 });
 
-router.get('/:id', function(req, res) {
+router.get('/:id', (req, res) => {
   res.send(people.find((p) => req.params.id === p.id));
 });
 
