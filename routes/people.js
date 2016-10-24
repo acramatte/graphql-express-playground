0c45e185895819624385b173a9d4457d6ddb73ed
Alexis Cramatte <alexis.cramatte@comerge.net>
2016-10-24 22:22:05 +0200

    persist people in POST

--- routes/people.js
+++ routes/people.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs')
 const people = require('../people.json');
 
 router.get('/', (req, res) => {
@@ -9,5 +10,14 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   res.send(people.find((p) => req.params.id === p.id));
 });
+
+router.post('/', (req, res) => {
+  saveUser(req.body, () => res.send('saved'));
+});
+
+function saveUser (user, callback) {
+  people.push(user);
+  fs.writeFile('people.json', JSON.stringify(people), (err) => callback());
+}
 
 module.exports = router;
