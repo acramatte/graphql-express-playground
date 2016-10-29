976ab03b3d4d47290f73481791e99a8c7ad02e34
Alexis Cramatte <alexis.cramatte@comerge.net>
2016-10-29 20:36:09 +0200

    add REST API support for deleting person

--- routes/people.js
+++ routes/people.js
@@ -13,13 +13,23 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  saveUser(req.body, () => res.send('saved'));
+  saveUser(req.body, (user) => res.send(user));
+});
+
+router.delete('/:id', (req, res) => {
+  deletePerson(req.params.id, (user) => res.send(user));
 });
 
 function saveUser (user, callback) {
   user.id = shortid.generate();
   people.push(user);
-  fs.writeFile('people.json', JSON.stringify(people), (err) => callback());
+  fs.writeFile('people.json', JSON.stringify(people), (err) => callback(user));
+}
+
+function deletePerson (id, callback) {
+  const person = people.find((p) => id === p.id);
+  const newPeoples = people.filter((p) => id !== p.id);
+  fs.writeFile('people.json', JSON.stringify(newPeoples), (err) => callback(person));
 }
 
 module.exports = router;
