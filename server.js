3827c496b15ccd27faf435d0b35ab0ab174e7d21
Alexis Cramatte <alexis.cramatte@comerge.net>
2016-11-09 23:57:27 +0100

    allows CORS from *

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const bodyParser = require('body-parser')
 const root = { hello: () => 'Hello world!' };
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/people', peopleRoutes);
